6c6ac7ae7b5b34e31381b8836969e83121d24e89
akameco <akameco.t@gmail.com>
2015-08-05 20:26:01 +0900

    Plugin

--- src/list-statuses.js
+++ src/list-statuses.js
@@ -1,42 +1,144 @@
 "use strict";
 
-import {T} from './config';
+import Twitter from 'twit';
 import {EventEmitter} from 'events';
 
-let list_id = 106243757;
-
-let event = new EventEmitter;
-
-event.on('data', (obj) => {
-  console.log(obj);
-
-  for (let o of obj['media']) {
-    console.log(o);
-  }
+export let T = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-T.get('lists/statuses', {list_id: list_id, count: 20}, (err, data, response) => {
-  if (err) {
-    console.log(err);
+class Timeline extends EventEmitter {
+
+  constructor (momo) {
+    super();
+    this.momo = momo;
+    this.count = this.momo.count;
+    this.list_id = this.momo.list_id;
   }
 
-  for (let tweet of data) {
-    if (tweet.extended_entities) {
-      let obj = {
-        id: tweet.id,
-        user_id: tweet.user.id,
-        username: tweet.user.name,
-        screen_name: tweet.user.screen_name,
-        text: tweet.text,
-        media: []
-      };
+  run() {
+    T.get('lists/statuses',
+        {list_id: this.list_id, count: this.count}, (err, data, response) => {
 
-      // 複数画像
-      for (let media of tweet.extended_entities.media) {
-        obj['media'].push(media.media_url);
+      if (err) {
+        console.log(err);
       }
 
-      event.emit('data', obj);
+      for (let tweet of data) {
+        if (tweet.extended_entities) {
+          let obj = {
+            id: tweet.id,
+            user_id: tweet.user.id,
+            username: tweet.user.name,
+            screen_name: tweet.user.screen_name,
+            text: tweet.text,
+            media: []
+          };
+
+          // 複数画像
+          for (let media of tweet.extended_entities.media) {
+            obj['media'].push(media.media_url);
+          }
+
+          // this.emit('tweet', obj);
+          this.momo.receive(obj);
+        }
+      }
+    });
+  }
+}
+
+// イベントが発生時に呼べれるリスナ
+class Listener {
+  // momo メイン
+  // cb コールバック関数
+  constructor(momo, cb) {
+    this.momo = momo;
+    this.cb = cb;
+  }
+
+  call(msg) {
+    this.cb(new Response(this.momo, msg));
+  }
+}
+
+// レスポンスクラス
+// イベントが発生すると作られる
+class Response {
+  constructor (momo, msg) {
+    this.momo = momo;
+    this.msg = msg;
+  }
+
+  get username() {
+    return this.msg.username;
+  }
+
+  get text() {
+    return this.msg.text;
+  }
+
+  get media() {
+    return this.msg.media;
+  }
+}
+
+// メイン関数
+class Momonic {
+  listeners = [];
+
+  // TODO: リストのidを選択可能にする
+  constructor (count = 20, list_id = 106243757) {
+    this.count = count;
+    this.list_id = list_id;
+  }
+
+  // リスナを登録する
+  push(cb) {
+    this.listeners.push(new Listener(this, cb));
+  }
+
+  // 登録されている全てのリスナを実行する
+  receive(msg) {
+    for(let listener of this.listeners) {
+      listener.call(msg);
     }
   }
+
+  // TODO: ファイルからプラグインのロード
+  load() { }
+  run() { }
+}
+
+let momo = new Momonic();
+
+// リスナを登録
+
+// momo.push((res) => {
+  // console.log(res.msg);
+// });
+
+momo.push((res) => {
+  console.log(res.username);
 });
+
+momo.push((res) => {
+  for(let i of res.media) {
+    console.log(i);
+  }
+});
+
+let tl = new Timeline(momo);
+
+// tl.on('tweet', (data) => {
+  // console.log(`@${data.screen_name}`);
+  // console.log(data.text);
+  // for (let o of data['media']) {
+    // console.log(o);
+  // }
+// });
+
+tl.run();
