483827c3527d6413da16e25472d137d999a4d00a
akameco <akameco.t@gmail.com>
2015-08-05 17:45:04 +0900

    Update

--- src/list-statuses.js
+++ src/list-statuses.js
@@ -1,41 +1,42 @@
 "use strict";
 
 import {T} from './config';
-
-//T.get('lists/list', {screen_name: screen_name, reverse: true}, (err, data, response) => {
-//
-//    if (err)
-//        cons
-//    }
-//
-//    for (let list of data) {
-//        if (list.name === '絵師') {
-//            list_id = list.id;
-//        }
-//    }
-//});
+import {EventEmitter} from 'events';
 
 let list_id = 106243757;
 
+let event = new EventEmitter;
+
+event.on('data', (obj) => {
+  console.log(obj);
+
+  for (let o of obj['media']) {
+    console.log(o);
+  }
+});
+
 T.get('lists/statuses', {list_id: list_id, count: 20}, (err, data, response) => {
-    if (err) {
-        console.log(err);
+  if (err) {
+    console.log(err);
+  }
+
+  for (let tweet of data) {
+    if (tweet.extended_entities) {
+      let obj = {
+        id: tweet.id,
+        user_id: tweet.user.id,
+        username: tweet.user.name,
+        screen_name: tweet.user.screen_name,
+        text: tweet.text,
+        media: []
+      };
+
+      // 複数画像
+      for (let media of tweet.extended_entities.media) {
+        obj['media'].push(media.media_url);
+      }
+
+      event.emit('data', obj);
     }
-
-    for (let tweet of data) {
-        if (tweet.extended_entities) {
-
-            console.log(tweet.extended_entities.media.length);
-
-            console.log(`${tweet.user.name}(@${tweet.user.screen_name}) ${tweet.text}`);
-
-            // 複数画像
-            for (let media of tweet.extended_entities.media) {
-                console.log(media.media_url)
-            }
-
-            console.log("\n");
-        }
-    }
-
+  }
 });
