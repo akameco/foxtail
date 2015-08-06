f3ed7e4f4152b2fbb3823d39b3eadb3446a1054a
akameco <akameco.t@gmail.com>
2015-08-06 10:22:01 +0900

    Add load Script

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 "use strict";
 
+import Fs from 'fs';
+import Path from 'path';
 import Twitter from 'twit';
 import T from './config';
-import {EventEmitter} from 'events';
 import Response from './response';
 import Listener from './listener';
 import {StreamingListTimeLine,ListTimeLine} from './timeline';
@@ -11,13 +12,14 @@ class Momonic {
     listeners = [];
 
     // TODO: リストのidを選択可能にする
-    constructor(count = 20, list_id = 106243757) {
+    constructor(count = 50, list_id = 106243757) {
         this.count = count;
         this.list_id = list_id;
+        this.load();
     }
 
     // リスナを登録する
-    push(cb) {
+    add(cb) {
         this.listeners.push(new Listener(this, cb));
     }
 
@@ -28,8 +30,26 @@ class Momonic {
         }
     }
 
-    // TODO: ファイルからプラグインのロード
     load() {
+        let path = Path.resolve(__dirname, './plugin');
+        Fs.readdir(path, (err, list) => {
+            for (let file of list) {
+                this.loadScript(path, file);
+            }
+        });
+    }
+
+    loadScript(path, file) {
+        let ext = Path.extname(file);
+        let full = Path.join(path, Path.basename(file, ext));
+        try {
+            let script = require(full);
+            if (typeof script === 'function') {
+                script(this);
+            }
+        } catch (e) {
+            console.log('Error' + e);
+        }
     }
 
     // TODO: 初期化処理
@@ -39,22 +59,6 @@ class Momonic {
 
 let momo = new Momonic();
 
-// リスナを登録
-
-// momo.push((res) => {
-// console.log(res.msg);
-// });
-
-momo.push((res) => {
-    console.log(res.username);
-});
-
-momo.push((res) => {
-    for (let i of res.media) {
-        console.log(i);
-    }
-});
-
-// let tl = new ListTimeLine(momo);
-let tl = new StreamingListTimeLine(momo);
+let tl = new ListTimeLine(momo);
+// let tl = new StreamingListTimeLine(momo);
 tl.run();
