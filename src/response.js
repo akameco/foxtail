4226ba65284991b91308d19dc0e261336c07c2ec
akameco <akameco.t@gmail.com>
2015-08-07 16:01:32 +0900

    Update response.js

--- src/response.js
+++ src/response.js
@@ -2,25 +2,33 @@
 // イベントが発生すると作られる
 
 export default class Response {
-    constructor(momo, msg) {
+
+    constructor(momo, tweet) {
         this.momo = momo;
-        this.msg = msg;
+        this.tweet = tweet;
     }
 
     get username() {
-        return this.msg.username;
+        return this.tweet.user.name;
     }
 
     get screen_name() {
-        return this.msg.screen_name;
+        return this.tweet.user.screen_name;
     }
 
     get text() {
-        return this.msg.text;
+        return this.tweet.text;
     }
 
     get media() {
-        return this.msg.media;
+        let images = [];
+        // 複数画像
+        if (this.tweet.extended_entities) {
+            for (let m of tweet.extended_entities.media) {
+                images.push(m.media_url);
+            }
+        }
+        return images;
     }
 
     // TODO: 各アクションの実装
