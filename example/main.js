c3841420827ca6ef28a3b76ce486531c86902427
akameco <akameco.t@gmail.com>
2015-09-10 09:59:44 +0900

    Ver 0.0.91

--- example/main.js
+++ example/main.js
@@ -1,6 +1,13 @@
-import FoxTail from '../lib/foxtail';
-import Path from 'path';
+import FoxTail from '../lib/foxtail'
+import Path from 'path'
 
-let fox = new FoxTail();
-fox.load(Path.resolve(__dirname, 'plugin'));
-fox.run();
+const config = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
+
+let fox = new FoxTail(config)
+fox.load(Path.resolve(__dirname, 'plugin'))
+fox.run()
