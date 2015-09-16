6a73c81a2f58c2b0dfb88a06a89e83705e3d44ac
akameco <akameco.t@gmail.com>
2015-09-17 00:52:06 +0900

    Remove sample

--- example/plugin/show.js
+++
@@ -1,8 +0,0 @@
-export default (fox) => {
-  fox.add((res) => {
-    let name = res.user_name
-    let screen_name = res.screen_name
-    let text = res.text
-    console.log(`@${screen_name}(${name}) ${text}\n`)
-  })
-}
--- example/plugin/username.js
+++
@@ -1,5 +0,0 @@
-export default fox => {
-  fox.add(res => {
-    console.log(res.user_name)
-  })
-}
